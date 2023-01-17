b83a6758f839b8ceaa18bd5175614821f0b3d3e0
Everton Paulino <evertonpaulino25@gmail.com>
2023-01-16 22:00:36 -0300

    inicial  models

---
+++ Certifique-se-api/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certifique_se_api.Models
+{
+    public class Student
+    {
+    }
+}
